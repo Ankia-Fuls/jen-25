8b3dbd7dde6bd950c6a450602564b2ba53e1bb95
Ankia-Fuls <ankiafuls01@gmail.com>
2026-07-23 15:44:00 +0200

    Decorations added

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
 import "../css/start.scss";
 import ToJen from "../components/StartPage/ToJenSVG.jsx";
-import QifreyHat from "../components/Decorations/QifreyHat.jsx";
+
+import { QifreyHat, OllyHat, GirlsHat } from "../components/Decorations/Hats.jsx";
+import { Star1, Star2, Star3, Star4 } from "../components/Decorations/Stars.jsx";
+import { Brushbuddy1, Brushbuddy2, Brushbuddy3, Brushbuddy4, Brushbuddy5 } from "../components/Decorations/BrushBuddy.jsx";
+import { Girls } from "../components/Decorations/Other.jsx";
+
 
 function Start({ setState }) {
   const Add = () => {
@@ -14,7 +19,7 @@ function Start({ setState }) {
       <span className="starting-message">
         Let the hunt <button onClick={Add}>begin</button>
       </span>
-      {/* <QifreyHat /> */}
+      <Girls />
     </section>
   );
 }
